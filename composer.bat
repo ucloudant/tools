6b3185f532f386ab810a08ca05b734b7999fcf26
shenyu <jacky.shenyu@gmail.com>
2019-11-06 18:03:57 +0800

    增加composer的支持

--- composer.bat
+++ composer.bat
@@ -1,7 +1,7 @@
 @echo off
-call choco list --local | find /i "composer" > nul
-if errorlevel 1 (
-call choco install -y composer
-call refreshenv
+if not exist %~dp0composer.phar (
+call php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+call php composer-setup.php --install-dir=%~dp0
+call php -r "unlink('composer-setup.php');"
 )
-php %ALLUSERSPROFILE%\ComposerSetup\bin\composer.phar %*
+php %~dp0composer.phar %*
