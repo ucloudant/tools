da218bbe7b44f372c87457f124df7fa28b8364b5
shenyu <jacky_shenyu@163.com>
2019-12-21 01:35:15 +0800

    更换Google Depot Tools远程仓库到GitHub

--- gclient.bat
+++ gclient.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist %~dp0depot_tools\gclient.py (
-call git clone https://code.aliyun.com/ishenyu/google-depot_tools.git %~dp0depot_tools
+call git clone https://github.com/ucloudant/depot_tools.git %~dp0depot_tools
 )
 set path=%path%;%~dp0depot_tools
 py -2 %~dp0depot_tools\gclient.py %*
