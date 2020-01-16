beef19aef212826be5ac7d55fc39e6bfaff81757
shenyu <jacky.shenyu@gmail.com>
2020-01-16 09:45:48 +0800

    解除composer内存限制

--- composer.bat
+++ composer.bat
@@ -4,4 +4,5 @@ call php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 call php composer-setup.php --install-dir=%~dp0
 call php -r "unlink('composer-setup.php');"
 )
+set COMPOSER_MEMORY_LIMIT=-1
 php %~dp0composer.phar %*
